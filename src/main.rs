8a198ffe55d8f7e7f158c7865a5520a811869963
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2022-10-21 01:41:38 +0200

    added digits in lightbulbs

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,15 @@ use colored::Colorize;
 use std::process::{exit, Command};
 use simple_scheduler::{Duration, Schedule, ScheduleAt, ScheduledTask, task};
 // Constants
-const DIGITS : [[&str; 11]; 7] = [
-    ["┏━━━┓","  ╻  ","┏━━━┓", "┏━━━┓","╻   ╻","┏━━━┓","┏━━━┓","┏━━━┓","┏━━━┓","┏━━━┓","     "],
-    ["┃   ┃","  ┃  ","    ┃", "    ┃","┃   ┃","┃    ","┃    ","    ┃","┃   ┃","┃   ┃","  ╻  "],
-    ["┃   ┃","  ┃  ","    ┃", "    ┃","┃   ┃","┃    ","┃    ","    ┃","┃   ┃","┃   ┃","     "],
-    ["┃   ┃","  ┃  ","┏━━━┛", "┣━━━┫","┗━━━┫","┗━━━┓","┣━━━┓","    ┃","┣━━━┫","┗━━━┫","     "],
-    ["┃   ┃","  ┃  ","┃    ", "    ┃","    ┃","    ┃","┃   ┃","    ┃","┃   ┃","    ┃","     "],
-    ["┃   ┃","  ┃  ","┃    ", "    ┃","    ┃","    ┃","┃   ┃","    ┃","┃   ┃","    ┃","  ╹  "],
-    ["┗━━━┛","  ╹  ","┗━━━━", "┗━━━┛","    ╹","┗━━━┛","┗━━━┛","    ╹","┗━━━┛","┗━━━┛","     "],
+const DIGITS : [[&str; 11]; 8] = [
+    ["","","","","","","","","","",""],
+    ["┏━━━┓","  ╻  ","┏━━━┓","┏━━━┓","╻   ╻","┏━━━┓","┏━━━┓","┏━━━┓","┏━━━┓","┏━━━┓","     "],
+    ["┃   ┃","  ┃  ","    ┃","    ┃","┃   ┃","┃    ","┃    ","    ┃","┃   ┃","┃   ┃","  ╻  "],
+    ["┃   ┃","  ┃  ","    ┃","    ┃","┃   ┃","┃    ","┃    ","    ┃","┃   ┃","┃   ┃","     "],
+    ["┃   ┃","  ┃  ","┏━━━┛","╺━━━┫","┗━━━┫","┗━━━┓","┣━━━┓","    ┃","┣━━━┫","┗━━━┫","     "],
+    ["┃   ┃","  ┃  ","┃    ","    ┃","    ┃","    ┃","┃   ┃","    ┃","┃   ┃","    ┃","     "],
+    ["┃   ┃","  ┃  ","┃    ","    ┃","    ┃","    ┃","┃   ┃","    ┃","┃   ┃","    ┃","  ╹  "],
+    ["┗━━━┛","  ╹  ","┗━━━━","┗━━━┛","    ╹","┗━━━┛","┗━━━┛","    ╹","┗━━━┛","┗━━━┛","     "],
 ];
 // Main
 fn main() {
@@ -39,15 +40,15 @@ fn clear() {
 // fn bulb
 fn bulb (dig: &str, index: u8) -> String {
     match index {
-        0 => String::from("  ----^----  "),
-        1 => format!(" |  {}  | ", dig.truecolor(249,212,102)),
-        2 => format!(" |  {}  | ", dig.truecolor(249,212,102)),
-        3 => format!(" |  {}  | ", dig.truecolor(249,212,102)),
-        4 => format!(" |  {}  | ", dig.truecolor(249,212,102)),
-        5 => format!(" |  {}  | ", dig.truecolor(249,212,102)),
-        6 => format!(" |  {}  | ", dig.truecolor(249,212,102)),
-        7 => format!("  \\ {} /  ", dig.truecolor(249,212,102)),
-        8 => String::from("  |||||||||  "),
+        0 => String::from("  ---^---  "),
+        1 => format!(" | {} | ", dig.truecolor(249,212,102)),
+        2 => format!(" | {} | ", dig.truecolor(249,212,102)),
+        3 => format!(" | {} | ", dig.truecolor(249,212,102)),
+        4 => format!(" | {} | ", dig.truecolor(249,212,102)),
+        5 => format!(" | {} | ", dig.truecolor(249,212,102)),
+        6 => format!(" | {} | ", dig.truecolor(249,212,102)),
+        7 => format!("  \\{}/  ", dig.truecolor(249,212,102)),
+        8 => String::from("  |||||||  "),
         _ => {
             println!("Error aborting");
             exit(1);
@@ -59,18 +60,32 @@ fn clock () {
     clear();
     let now = Local::now();
     let time = now.format("%H:%M:%S");
+    let mut index: u8 = 0;
     // print bulb
-    for a in 0..9 {
-        for _b in 0..8 {
-            print!("{}", bulb("┏━━━┓", u8::from(a)))
+    for row in &DIGITS {
+        for char in time.to_string().chars() {
+            let column = match char {
+                '0'..='9' => char as usize - '0' as usize,
+                ':' => 10,
+                _ => {
+                    println!("Error aborting");
+                    exit(1);
+                }
+            };
+            print!("{} ", bulb(row[column], index));
         }
+        index += 1;
         println!();
     }
+    for _i in 0..8 {
+        print!("{} ", bulb("", u8::from(8)));
+    }
+    println!();
     // Box
     let date = now.format("%A :: %d %b %Y");
-    let _mid = format!("{}", date.to_string().pad_to_width_with_alignment(68, Alignment::Middle).truecolor(249,212,102));
+    let _mid = format!("{}", date.to_string().pad_to_width_with_alignment(93, Alignment::Middle).truecolor(249,212,102));
     let mid = format!("┃{}┃", _mid);
-    println!("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+    println!("┃▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔┃");
     println!("{}", mid);
-    println!("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
+    println!("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
 }
