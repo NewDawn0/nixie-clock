b82e3a5f8d33530da2a3e9ce48f13230f62a8815
NewDawn0 <newdawn.v0.0@gmail.com>
2024-04-19 01:31:30 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A commandline clock with nixie tubes";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    nix-systems.url = "github:nix-systems/default";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      eachSystem = nixpkgs.lib.genAttrs (import inputs.nix-systems);
+      mkPkgs = system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ inputs.rust-overlay.overlays.default ];
+        };
+    in {
+      packages = eachSystem (system:
+        let pkgs = mkPkgs system;
+        in {
+          default = pkgs.rustPlatform.buildRustPackage {
+            name = "nixie-clock";
+            version = "0.0.1";
+            cargoLock.lockFile = ./Cargo.lock;
+            src = pkgs.lib.cleanSource ./.;
+            meta = with pkgs.lib; {
+              description = "A commandline clock with nixie tubes";
+              homepage = "https://github.com/NewDawn0/nixie-clock";
+              maintainers = with maintainers; [ "NewDawn0" ];
+              license = licenses.mit;
+            };
+          };
+        });
+    };
+}
